7c7671f907bacfbf5b5160e113a3da547c794ea6
Jondoloh <jndolo@aimsammi.org>
2022-04-26 01:35:33 +0000

    six time

---
+++ config.py
@@ -0,0 +1,10 @@
+import argparse
+
+args = argparse.Namespace(
+    lr = 1e-4,
+    bs = 8,
+    train_size = 0.8,
+    wd=1.0,
+    path = '/Users/johnndolo/Desktop/Deep Learning/data/Images',
+    metadata = '/Users/johnndolo/Desktop/Deep Learning/data/metadata_ok.csv' 
+)
